1a32e769a0d1500c9127034dd1235d4d92ba6e7e
Dmitry Pytkin <tutbaker@gmail.com>
2015-12-16 18:54:10 +0600

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,115 @@
+/* global FastClick, renderIcons, Slideout, ymaps, alert */
+
+$(function () {
+	'use strict';
+
+	// Attach fastclick for mobiles (https://github.com/ftlabs/fastclick)
+	FastClick.attach(document.body);
+
+
+
+	if ($('.offices-list .map-holder').length > 0) {
+		$.getScript('http://api-maps.yandex.ru/2.1/?lang=ru_RU', function () {
+			ymaps.ready(function () {
+				$('.offices-list .map-holder').each(function () {
+					var address = $(this).data('address');
+					var mapHolderID = $(this).attr('id');
+
+					ymaps.geocode(address, { results: 1 }).then(function (res) {
+						// Выбираем первый результат геокодирования.
+						var firstGeoObject = res.geoObjects.get(0);
+						var myPlacemark = new ymaps.Placemark(firstGeoObject.geometry.getCoordinates(), { content: address });
+
+						// Создаем карту с нужным центром.
+						var map = new ymaps.Map(mapHolderID, {
+							center: firstGeoObject.geometry.getCoordinates(),
+							zoom: 16,
+							controls: ['zoomControl']
+						});
+
+						map.behaviors.disable(['drag', 'rightMouseButtonMagnifier', 'leftMouseButtonMagnifier', 'multiTouch', 'dblClickZoom', 'scrollZoom']);
+						map.geoObjects.add(myPlacemark);
+					}, function (err) {
+						// Если геокодирование не удалось, сообщаем об ошибке.
+						alert(err.message);
+					});
+				});
+			});
+		});
+	}
+
+	// Main slider
+	$('.main-promo-slider').owlCarousel({
+		slideSpeed: 400,
+		loop: true,
+		autoWidth: true,
+		pagination: true,
+		navigation: false,
+		items: 1,
+		singleItem: true
+	});
+
+	$('.main-our-works .works-slider, .type-page-our-works .works-slider').owlCarousel({
+		slideSpeed: 400,
+		loop: true,
+		autoWidth: true,
+		pagination: false,
+		navigation: true,
+		navigationText: ['<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>'],
+		items: 1,
+		singleItem: true,
+		afterInit: function (elem) {
+			renderIcons($(elem).get(0));
+			$(elem).after('<div class="slider-counter"><strong>1</strong> / ' + $(elem).find('.item').length + '</div>');
+		},
+		afterMove: function () {
+			$(this.$elem[0]).parents().find('.slider-counter strong').text(this.currentItem + 1);
+		}
+	});
+
+	(function () {
+		var slideout = new Slideout({
+			panel: $('.site-wrapper').get(0),
+			menu: $('.slideout-menu').get(0),
+			padding: 230,
+			tolerance: 70
+		});
+
+		$('.js-slideout-handler').on('click', function (event) {
+			event.preventDefault();
+			slideout.toggle();
+		});
+	})();
+
+	// User reviews
+	$('.user-reviews .reviews-slider').owlCarousel({
+		slideSpeed: 400,
+		loop: true,
+		autoWidth: true,
+		pagination: false,
+		navigation: true,
+		navigationText: ['<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>'],
+		items: 1,
+		singleItem: true,
+		afterInit: function (elem) {
+			renderIcons($(elem).get(0));
+			$(elem).before('<div class="slider-counter"><span data-icon="comment-count-circle"></span><span class="count"><strong>1</strong> / ' + $(elem).find('.item').length + '</span></div>');
+			renderIcons($(elem).prev('.slider-counter').get(0));
+		},
+		afterMove: function () {
+			$(this.$elem[0]).parents().find('.slider-counter strong').text(this.currentItem + 1);
+		}
+	});
+
+	// Action slider
+	$('.type-action-promo .action-slider').owlCarousel({
+		slideSpeed: 400,
+		loop: true,
+		autoWidth: true,
+		pagination: true,
+		navigation: false,
+		items: 1,
+		singleItem: true
+	});
+
+});
---
+++ app/js/icon-loader.js
@@ -0,0 +1,95 @@
+(function (document) {
+	'use strict';
+
+	function icon(name, options) {
+		var optionsz = options || {};
+		var size    = optionsz.size ? 'is-' + optionsz.size : '';
+		var klass   = 'icon icon-' + name + ' ' + size + '' + (optionsz.class || '');
+
+		var iconz   = '<svg class="icon__cnt">' +
+						'<use xlink:href="#icon-' + name + '" />' +
+						'</svg>';
+
+		var html =  '<div class="' + klass + '">' +
+						wrapSpinner(iconz, klass) +
+					'</div>';
+
+		return html;
+	}
+	function wrapSpinner(html, klass) {
+		if (klass.indexOf('spinner') > -1) {
+			return '<div class="icon__spinner">' + html + '</div>';
+		} else {
+			return html;
+		}
+	}
+
+	var renderIcons = function (inst) {
+		var icons = inst ? inst.querySelectorAll('[data-icon]') : document.querySelectorAll('[data-icon]');
+
+		for (var i = 0; i < icons.length; i++) {
+			var currentIcon = icons[i];
+			var name        = currentIcon.getAttribute('data-icon');
+			var options = {
+				class:  currentIcon.className,
+				size:   currentIcon.getAttribute('data-size')
+			};
+
+			currentIcon.insertAdjacentHTML('beforebegin', icon(name, options));
+			currentIcon.parentNode.removeChild(currentIcon);
+		}
+	};
+	function renderSprite(path) {
+		var file = (path !== '' && typeof path !== 'undefined') ? path : 'img/sprites/svg_sprite.svg';
+		var revision = 1450256115;
+		if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect) {
+			document.createElement('svg');
+			document.createElement('use');
+		}
+		var isLocalStorage = 'localStorage' in window && window.localStorage !== null;
+		var request;
+		var data;
+		var insertIT = function () {
+			var sprite = '<div style="display: none">' + data + '</div>';
+			document.body.insertAdjacentHTML('afterbegin', sprite);
+		};
+		var insert = function () {
+			if (document.body) {
+				insertIT();
+			} else {
+				document.addEventListener('DOMContentLoaded', insertIT);
+			}
+		};
+
+		if (isLocalStorage && parseInt(localStorage.getItem('inlineSVGrev'), 10) === revision) {
+			data = localStorage.getItem('inlineSVGdata');
+			if (data) {
+				insert();
+				return true;
+			}
+		}
+		try {
+			request = new XMLHttpRequest();
+			request.open('GET', file, true);
+			request.onload = function () {
+				if (request.status >= 200 && request.status < 400) {
+					data = request.responseText;
+					insert();
+					if (isLocalStorage) {
+						localStorage.setItem('inlineSVGdata', data);
+						localStorage.setItem('inlineSVGrev', revision);
+					}
+				}
+			};
+			request.send();
+		} catch (e) {}
+	}
+
+	document.addEventListener('DOMContentLoaded', function () {
+		renderSprite();
+		renderIcons();
+	});
+
+	window.renderIcons = renderIcons;
+
+})(window.document);
